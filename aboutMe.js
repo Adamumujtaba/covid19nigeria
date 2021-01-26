e74e7175ab31c63d5df5ce4357f8c6b9f844cf78
Adamumujtaba <47597665+Adamumujtaba@users.noreply.github.com>
2021-01-26 18:36:32 +0100

    Add files via upload

---
+++ aboutMe.js
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>About Me</title>
+</head>
+<body>
+
+
+    <!--Header-->
+    <header>
+        <div class="container">
+                
+                    <img src="Images/coat.png" id="coat"  alt="logo">
+                    <div class="header_text">
+                    <h2>Covid-19 <span>Nigeria</span></h2>
+                    </div>
+                   <img src="Images/ncdc.jpg" id="ncdc"  alt="NCDC logo">
+         
+        </div>    
+        </header>
+    
+    <!-- End of Header  -->
+    
+        
+        
+    <!----------------Navigation Bar----------------->
+        <nav>
+            <div class="container">
+                <input type="checkbox" id="bar">
+                 <label for="bar">
+                
+            <div class="bar">
+                <div class="bars"></div>
+                <div class="bars"></div>
+                <div class="bars"></div>
+            </div>
+                    
+            </label>
+               
+                <div class="nav">
+                <ul>
+                    <a href="./index.js"><li>Home</li></a>
+                    <a href="./#state"><li>State</li></a>
+                    <a href="./#map"><li>Map</li></a>
+                    <a href="https://covid19.ncdc.gov.ng/" target="_blank"><li>NCDC</li></a>
+                    <a href="./aboutMe.js"><li>About</li></a>
+              
+                </ul>
+                </div>
+                
+            </div>
+        </nav>
+       
+    <!----------------End of Navigation bar--------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-----------About me -->
+    <div class="about">
+        <div class="container">
+            <div>
+                <img src="Images/me.JPG" width="100%" alt="Mujtaba Adamu" />
+            </div>
+
+            <div>
+            <h1>About Me</h1>
+            <h3>Hi i'm Mujtaba Adamu </h3>
+
+            <p>
+                Student of Modibbo Adama University 
+                of Technology Yola studying computer science, and also a web developer.
+
+            </p>
+            
+            <ul>
+                <a href="mailto:mujtabaadamu1998@gmail.com"><li><i class="fa fa-envelope"></i></a></li>
+                <a href="https://wa.me/8068279731" id="whatsapp"><li><i class="fa fa-whatsapp"></i></a></li>
+                <a href="https://twitter.com/Mujtaba__ibb?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @Mujtaba__ibb<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></a>        
+                <a href="https://web.facebook.com/mujtaba.adamu.77" id="fb"><li><i class="fa fa-facebook"></i></a></li>
+
+            </ul>
+        </div>
+        
+        </div>
+
+    </div>
+
+
+
+                  
+
+
+    <footer>
+        <div class="container">
+            
+            <div>
+                <h2>Quick Links</h2>
+                <ul>
+                    
+                    <a href="./index.js"><li>Home</li></a>
+                    <a href="#state"><li>State</li></a>
+                    <a href="#map"><li>Map</li></a>
+                    <a href="https://covid19.ncdc.gov.ng/" target="_blank"><li>NCDC</li></a>
+                    <a href="aboutMe.js"><li>About Me</li></a>
+                
+            </ul>
+            </div>
+
+            <div>
+                <h2>NCDC Connect Center</h2>
+                <ol>
+                    <li><i class="fa fa-phone"> </i> 0800 9700 0010, 6232</li>
+                    <li><i id="whatsapp" class="fa fa-whatsapp"> </i> +2347087110839</li>
+                    <li><i class="fa fa-envelope"> </i> Sms +2348099555577</li>
+                </ol>
+            </div>
+            
+
+            <div>
+                <h2>Contact Me</h2>
+                <p>
+                
+                </p>
+                <ol class="social">
+             
+                    <li><a id="email" href="mailto:mujtabaadamu1998@gmail.com"><i class="fa fa-envelope"></i></a></li>
+                    <li><a id="whatsapp" href="https://wa.me/8068279731"><i class="fa fa-whatsapp"></i></a></li>
+                   <li><a id="twitter" href="https://twitter.com/Mujtaba__ibb?ref_src=twsrc%5Etfw"><i class="fa fa-twitter"></i></a></li>
+                    <li><a id="fb" href="https://web.facebook.com/mujtaba.adamu.77"><i class="fa fa-facebook"></i></a></li>
+ 
+                </ol>
+            </div>
+
+        </div>
+
+    </footer>
+</body>
+</html>
